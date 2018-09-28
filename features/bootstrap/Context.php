f9b8542a68db581e995b83c17f2967ed5b551a37
Kévin Gomez <contact@kevingomez.fr>
2018-09-29 00:00:17 +0200

    Use RulerZ's behat context

--- features/bootstrap/Context.php
+++ features/bootstrap/Context.php
@@ -3,29 +3,13 @@
 declare(strict_types=1);
 
 use Behat\Behat\Context\Context as BehatContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Entity\Eloquent\Player;
+use RulerZ\Test\BaseContext;
 
-class Context implements BehatContext
+class Context extends BaseContext implements BehatContext
 {
-    /** @var \RulerZ\RulerZ */
-    private $rulerz;
-
-    /** @var mixed */
-    private $dataset;
-
-    /** @var array */
-    private $parameters = [];
-
-    /** @var array */
-    private $executionContext = [];
-
-    /** @var mixed */
-    private $results;
-
-    public function __construct()
+    public function initialize()
     {
         $capsule = new Capsule();
 
@@ -41,127 +25,18 @@ class Context implements BehatContext
     }
 
     /**
-     * Returns the compilation target to be tested.
+     * {@inheritdoc}
      */
-    private function getCompilationTarget(): \RulerZ\Compiler\CompilationTarget
+    protected function getCompilationTarget(): \RulerZ\Compiler\CompilationTarget
     {
         return new \RulerZ\Eloquent\Target\Eloquent();
     }
 
     /**
-     * Returns the default dataset to be filtered.
-     *
-     * @return mixed
+     * {@inheritdoc}
      */
-    private function getDefaultDataset()
+    protected function getDefaultDataset()
     {
         return Player::query();
-    }
-
-    /**
-     * Create a default execution context that will be given to RulerZ when
-     * filtering a dataset.
-     *
-     * @return mixed
-     */
-    private function getDefaultExecutionContext()
-    {
-        return [];
-    }
-
-    /**
-     * @Given RulerZ is configured
-     */
-    public function rulerzIsConfigured()
-    {
-        // compiler
-        $compiler = new \RulerZ\Compiler\Compiler(new \RulerZ\Compiler\EvalEvaluator());
-
-        // RulerZ engine
-        $this->rulerz = new \RulerZ\RulerZ($compiler, [$this->getCompilationTarget()]);
-    }
-
-    /**
-     * @When I define the parameters:
-     */
-    public function iDefineTheParameters(TableNode $parameters)
-    {
-        // named parameters
-        if (count($parameters->getRow(0)) !== 1) {
-            $this->parameters = $parameters->getRowsHash();
-
-            return;
-        }
-
-        // positional parameters
-        $this->parameters = array_map(function ($row) {
-            return $row[0];
-        }, $parameters->getRows());
-    }
-
-    /**
-     * @When I use the default execution context
-     */
-    public function iUseTheDefaultExecutionContext()
-    {
-        $this->executionContext = $this->getDefaultExecutionContext();
-    }
-
-    /**
-     * @When I use the default dataset
-     */
-    public function iUseTheDefaultDataset()
-    {
-        $this->dataset = $this->getDefaultDataset();
-    }
-
-    /**
-     * @When I filter the dataset with the rule:
-     */
-    public function iFilterTheDatasetWithTheRule(PyStringNode $rule)
-    {
-        $this->results = $this->rulerz->filter($this->dataset, (string) $rule, $this->parameters, $this->executionContext);
-
-        $this->parameters = [];
-        $this->executionContext = [];
-    }
-
-    /**
-     * @Then I should have the following results:
-     */
-    public function iShouldHaveTheFollowingResults(TableNode $table)
-    {
-        $results = iterator_to_array($this->results);
-
-        if (count($table->getHash()) !== count($results)) {
-            throw new \RuntimeException(sprintf("Expected %d results, got %d. Expected:\n%s\nGot:\n%s", count($table->getHash()), count($results), $table, var_export($results, true)));
-        }
-
-        foreach ($table as $row) {
-            foreach ($results as $result) {
-                $value = $this->fieldFromResult($result, 'pseudo');
-
-                if ($value === $row['pseudo']) {
-                    return;
-                }
-            }
-
-            throw new \RuntimeException(sprintf('Player "%s" not found in the results.', $row['pseudo']));
-        }
-    }
-
-    /**
-     * Fetches a field from a result.
-     *
-     * @param mixed $result
-     * @param string $field
-     *
-     * @return mixed
-     */
-    private function fieldFromResult($result, $field)
-    {
-        $objectResult = is_array($result) ? (object) $result : $result;
-
-        return $objectResult->$field;
     }
 }
